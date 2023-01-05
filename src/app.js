8b70b0e95fab0657156743e52989afeadfc1b299
Jeferson Patricio <jelsantos37@gmail.com>
2023-01-05 18:18:42 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/aulas', (req, res) => {
+    res.render('aulas');
+});
+app.get('/aprenda-agora', (req, res) => {
+    res.render('aprenda');
+});
+
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
